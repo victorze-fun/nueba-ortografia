0650b144ade3531fa8e01b0a8b363c3e1d3bd94a
victorze <victorzevallos@protonmail.com>
2019-09-18 10:21:22 -0500

    Add names to the tests

--- tests.js
+++ tests.js
@@ -1,54 +1,54 @@
 const { simplifySpelling } = require('./app')
 
-test('1', () => {
+test('LaLetra_v SeConvierteEn_b', () => {
   return simplifySpelling('venta VENTA') === 'benta BENTA'
 })
 
-test('2', () => {
+test('LaLetra_z SeConvierteEn_s', () => {
   return simplifySpelling('zapato ZAPATO') === 'sapato SAPATO'
 })
 
-test('3', () => {
+test('SeEliminaLa_h_Muda', () => {
   return simplifySpelling('hora HORA') === 'ora ORA'
 })
 
-test('3.1', () => {
+test('PalabrasQueEmpiezanCon_Ha_He_Hi_Ho_Hu SeConvierteEn_A_E_I_O_U', () => {
   return simplifySpelling('Haber Helado Higo Hongo Hugo') === 'Aber Elado Igo Ongo Ugo'
 })
 
-test('4', () => {
+test('EnPalabrasQueContienen_ch_SeConservaLa_h', () => {
   return simplifySpelling('chita CHITA Chita') === 'chita CHITA Chita'
 })
 
-test('5', () => {
+test('LaCombinacion_gue SeConvierteEn_ge', () => {
   return simplifySpelling('guerra GUERRA Guerra') === 'gerra GERRA Gerra'
 })
 
-test('5.1', () => {
+test('LaCombinacion_gué SeConvierteEn_gé', () => {
   return simplifySpelling('llegué LLEGUÉ Gué...') === 'llegé LLEGÉ Gé...'
 })
 
-test('6', () => {
+test('LaCombinacion_gui SeConvierteEn_gi', () => {
   return simplifySpelling('guitarra GUITARRA Guitarra') === 'gitarra GITARRA Gitarra'
 })
 
-test('6.1', () => {
+test('LaCombinacion_guí SeConvierteEn_gí', () => {
   return simplifySpelling('guía GUÍA Guí...') === 'gía GÍA Gí...'
 })
 
-test('7', () => {
+test('LaCombinacion_ge SeConvierteEn_je', () => {
   return simplifySpelling('ingeniería INGENIERÍA Gemelo') === 'injeniería INJENIERÍA Jemelo'
 })
 
-test('7.1', () => {
+test('LaCombinacion_gé SeConvierteEn_jé', () => {
   return simplifySpelling('género GÉNERO Génesis') === 'jénero JÉNERO Jénesis'
 })
 
-test('8', () => {
+test('LaCombinacion_gi SeConvierteEn_ji', () => {
   return simplifySpelling('agitar AGITAR Ginete') === 'ajitar AJITAR Jinete'
 })
 
-test('8.1', () => {
+test('LaCombinacion_gí SeConvierteEn_jí', () => {
   return simplifySpelling('logística LOGÍSTICA Gíralo') === 'lojística LOJÍSTICA Jíralo'
 })
 
